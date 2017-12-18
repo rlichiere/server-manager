21c518a65a1748774e5f217ba97253f145c79324
rlichiere <rlichiere@visiativ.com>
2017-12-18 03:05:14 +0100

    # 0.0.3 * improved hostmanager

--- hostmanager/utils/docker.py
+++ hostmanager/utils/docker.py
@@ -6,7 +6,9 @@ class DockerCompose(object):
     def __init__(self, application, environment):
         self._app = application
         self._env = environment
-        self._path = '%s/projects/%s/%s' % (settings.config.get('base_dir'), environment.name, application.name)
+        self._path_old = '%s/projects/%s/%s' % (settings.config.get('base_dir'), environment.name, application.name)
+        self._path = '%s/%s/%s' % (settings.config.get('projects_dir'), environment.name, application.name)
+        self._host_fqdn = settings.config.get('host_fqdn')
 
     def up(self):
         self._send_cmd('docker-compose up -d')
@@ -25,4 +27,7 @@ class DockerCompose(object):
 
 
     def _send_cmd(self, command):
-        shell.execute_cmd(command, working_dir=self._path, host_fqdn=settings.config.get('host_fqdn'))
+        print 'DockerCompose._send_cmd: command : %s' % command
+        print 'DockerCompose._send_cmd: self._path_old : %s' % self._path_old
+        print 'DockerCompose._send_cmd: self._path : %s' % self._path
+        shell.execute_cmd(command, working_dir=self._path, host_fqdn=self._host_fqdn)
